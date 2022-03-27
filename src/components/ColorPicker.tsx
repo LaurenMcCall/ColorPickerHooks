0f13e1882985196da5fdb7c93e7445ac77cb9d34
Lauren McCall <lmmccolloch@gmail.com>
2022-03-27 14:27:06 -0400

    random color button works

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
 
 export function ColorPicker() {
-  const [hue, setHue] = useState('303')
-  const [saturation, setSaturation] = useState('50')
-  const [lightness, setLightness] = useState('50')
-  const [alpha, setAlpha] = useState('100')
+  const [hue, setHue] = useState<string | number>('311')
+  const [saturation, setSaturation] = useState<string | number>('56')
+  const [lightness, setLightness] = useState<string | number>('50')
+  const [alpha, setAlpha] = useState<string | number>('100')
   const newBackgroundColor = `hsla(${hue},${saturation}%,${lightness}%, ${alpha}%)`
   const newStyle = { backgroundColor: newBackgroundColor }
 
+  function handleClickRandomColorButton() {
+    setHue(Math.floor(Math.random() * 360))
+    setSaturation(Math.floor(Math.random() * 101))
+    setLightness(Math.floor(Math.random() * 101))
+    setAlpha(Math.floor(Math.random() * 101))
+  }
   function handleHueRange(event: React.ChangeEvent<HTMLInputElement>) {
     event.preventDefault()
     setHue(event.target.value)
@@ -32,7 +38,9 @@ export function ColorPicker() {
           hsla({hue}, {saturation}%, {lightness}%, {alpha})
         </div>
       </header>
-
+      <div className="flex">
+        <button onClick={handleClickRandomColorButton}>Random Color</button>
+      </div>
       <body>
         <section>
           <input
