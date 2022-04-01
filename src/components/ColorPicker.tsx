08ea11720ea07240d329e835cd5b704f42eb1306
Lauren McCall <lmmccolloch@gmail.com>
2022-03-31 20:28:44 -0400

    sliders now show gradients of the random color

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -13,21 +13,24 @@ export function ColorPicker() {
   const [alpha, setAlpha] = useState<string | number>(
     Math.floor(Math.random() * 101)
   )
-  const newBackgroundColor = `hsla(${hue},${saturation}%,${lightness}%, ${alpha}%)`
+  const newBackgroundColor = `hsla(${hue},${saturation}%,${lightness}%, .${alpha})`
   const newStyle = { backgroundColor: newBackgroundColor }
-  // const minColor = { minColors: newStyle }
-  // const maxColor = { maxColors: newStyle }
-  // const newSaturation = newStyle
-  // const saturationGradient = {
-  //   background: `linear-gradient(to right, ${newSaturation} 0%, ${newSaturation}100%)`,
-  // }
-  // let color = newStyle
+
+  const satGradient = `-webkit-linear-gradient(45deg, hsla(${hue},0%,${lightness}%,.${alpha}), hsla(${hue},20%,${lightness}%,.${alpha}), hsla(${hue},40%,${lightness}%,.${alpha}), hsla(${hue},60%,${lightness}%,.${alpha}), hsla(${hue},80%,${lightness}%,.${alpha}), hsla(${hue},100%,${lightness}%,.${alpha}))`
+  const gradientSatStyle = { background: satGradient }
+
+  const satLightness = `-webkit-linear-gradient(45deg, hsla(${hue},${saturation}%,0%,.${alpha}), hsla(${hue},${saturation}%,20%,.${alpha}), hsla(${hue},${saturation}%,40%,.${alpha}), hsla(${hue},${saturation}%,60%,.${alpha}), hsla(${hue},${saturation}%,80%,.${alpha}), hsla(${hue},${saturation}%,100%,.${alpha}))`
+  const gradientLightStyle = { background: satLightness }
+
+  const satAlpha = `-webkit-linear-gradient(45deg, hsla(${hue},${saturation}%,${lightness}%,0), hsla(${hue},${saturation}%,${lightness}%,.20), hsla(${hue},${saturation}%,${lightness}%,.4), hsla(${hue},${saturation}%,${lightness}%,.6000000000000001), hsla(${hue},${saturation}%,${lightness}%,.8), hsla(${hue},${saturation}%,${lightness}%,1.0))`
+  const gradientAlphaStyle = { background: satAlpha }
 
   function handleClickRandomColorButton() {
     setHue(Math.floor(Math.random() * 360))
     setSaturation(Math.floor(Math.random() * 101))
     setLightness(Math.floor(Math.random() * 101))
     setAlpha(Math.floor(Math.random() * 101))
+    console.log(gradientLightStyle)
   }
 
   function handleHueRange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -50,7 +53,7 @@ export function ColorPicker() {
   return (
     <div>
       <head>
-        <h2>
+        <h2 className="stroke-text-h2">
           <img
             src="/images/mushroom.svg"
             alt="cute little mushroom"
@@ -67,14 +70,14 @@ export function ColorPicker() {
         <div style={newStyle}>
           <img
             // className="shadow"
-            src="images/teaparty.svg"
+            src="/images/teaparty.svg"
             alt="alice in wonderland tea party"
             width="100%"
           />
         </div>
       </header>
-      <aside>
-        We&apos;re all mad here
+      <aside className="stroke-text-aside">
+        we&apos;Re aLL mAd HeRe
         <img src="/images/mushroom.svg" alt="little mushroom" width="25px" />
       </aside>
       <div className="flex">
@@ -96,13 +99,13 @@ export function ColorPicker() {
         </section>
         <section>
           <input
-            style={newStyle}
+            style={gradientSatStyle}
             // style={{
-            //   background: `linear-gradient(to right, ${minColor}, ${maxColor})`,
+            //   background: satGradient,
             // }}
             // style={saturationGradient}
             value={saturation}
-            // className="saturation-slider"
+            // className="test"
             onChange={handleSaturationRange}
             type="range"
             id="Saturation"
@@ -114,7 +117,7 @@ export function ColorPicker() {
         </section>
         <section>
           <input
-            style={newStyle}
+            style={gradientLightStyle}
             value={lightness}
             // className="lightness-slider"
             onChange={handleLightnessRange}
@@ -128,15 +131,15 @@ export function ColorPicker() {
         </section>
         <section>
           <input
-            style={newStyle}
+            style={gradientAlphaStyle}
             value={alpha}
-            // className="alpha-slider"
+            className="test"
             onChange={handleAlphaRange}
             type="range"
             id="Alpha"
             name="Alpha"
-            min="0"
-            max="100"
+            min=".0"
+            max="1.0"
           />
           <label htmlFor="Alpha">Alpha: {alpha}</label>
         </section>
